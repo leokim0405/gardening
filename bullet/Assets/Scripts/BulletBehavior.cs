06bebc55c2cdccbbe63611878aaa5b34ef9a5729
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2019-12-26 03:38:24 +0900

    오브젝트 풀링 스크립트 연습

---
+++ bullet/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float activeTime = 3.0f;
+    public float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Spawn()
+    {
+        gameObject.SetActive(true);
+        spawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if(Time.time - spawnTime >= activeTime)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ bullet/Assets/Scripts/BulletPooler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPooler : MonoBehaviour
+{
+    public float coolTime = 0.1f;
+    public GameObject bullet;
+    public int poolCount = 0;
+
+    List<GameObject> bullets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullets = new List<GameObject>();
+        while(poolCount > 0)
+        {
+            GameObject obj = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            obj.SetActive(false);
+            bullets.Add(obj);
+            poolCount++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject bullet in bullets)
+        {
+            if(!bullet.activeInHierarchy)
+            {
+                bullet.transform.position = gameObject.transform.position;
+                bullet.GetComponent<BulletBehavior>().Spawn();
+                break;
+            }
+        }
+    }
+}
