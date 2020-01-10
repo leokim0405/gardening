05e27b2dfdf28834e527bcefba719ad0919beb64
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2020-01-11 00:17:03 +0900

    똥피하기 - 기본 캐릭터 움직임 , 배경 팔레트, 스프라이트 쪼개기

---
+++ unity practice/ddong/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private float speed = 8;
+    private float horizontal;
+
+    private new SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontal);
+        PlayerMove();
+        ScreenChk();
+    }
+
+    private void PlayerMove()
+    {
+        if(horizontal < 0)
+        {
+            renderer.flipX = true; // 뒤집기
+        }
+        else
+        {
+            renderer.flipX = false;
+        }
+        rigidbody.velocity = new Vector2(horizontal * speed, rigidbody.velocity.y);
+    }
+
+    private void ScreenChk()
+    {
+        Vector3 worlpos = Camera.main.WorldToViewportPoint(transform.position);
+        if(worlpos.x < 0.05f)
+        {
+            worlpos.x = 0.05f;
+        }
+        if(worlpos.x > 0.95f)
+        {
+            worlpos.x = 0.95f;
+        }
+        this.transform.position = Camera.main.ViewportToWorldPoint(worlpos);
+    }
+}
