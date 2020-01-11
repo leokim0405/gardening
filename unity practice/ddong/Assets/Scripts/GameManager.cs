50aecfd372c0c7967486a2712a3def8e21b6a3af
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2020-01-11 22:23:46 +0900

    똥피하기 똥 생성 및 낙하 충돌판정

---
+++ unity practice/ddong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject poop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Creatpoop();
+        StartCoroutine(CreatepoopRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator CreatepoopRoutine()
+    {
+        while (true)
+        {
+            Creatpoop();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void Creatpoop()
+    {
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.0f, 1.0f), 1.1f, 0) );
+        pos.z = 0.0f;
+        Instantiate(poop, pos, Quaternion.identity);
+    }
+}
---
+++ unity practice/ddong/Assets/Scripts/poop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
