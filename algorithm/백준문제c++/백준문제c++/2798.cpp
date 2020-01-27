cd9be278c5327d0c381c932804dc459762e6ed21
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2020-01-28 03:54:56 +0900

    백준 2798 수정

--- algorithm/백준문제c++/백준문제c++/2798.cpp
+++ algorithm/백준문제c++/백준문제c++/2798.cpp
@@ -9,14 +9,13 @@ int main()
 	cin >> N >> M; // N은 카드 개수 M은 최대한 가까운 수
 	int temp, sum;
 	vector<int> n_list;
-	int count = 0;
 
 	for (int i = 0; i < N; i++)
 	{
 		cin >> temp;
 		n_list.push_back(temp);
 	}
-	sum = n_list[0] + n_list[1] + n_list[2];
+	sum = 0;
 
 	for (int j = 0; j < n_list.size() - 2; j++)
 	{
@@ -24,8 +23,7 @@ int main()
 		{
 			for (int l = k+1; l < n_list.size(); l++)
 			{
-				count++;
-				cout << n_list[j] + n_list[k] + n_list[l] << endl;
+				//cout << n_list[j] + n_list[k] + n_list[l] << endl;
 				if ( (n_list[j] + n_list[k] + n_list[l] <= M) && (sum < n_list[j] + n_list[k] + n_list[l]) )
 				{
 					sum = n_list[j] + n_list[k] + n_list[l];
@@ -33,7 +31,6 @@ int main()
 			}
 		}
 	}
-	printf("%d\n", count);
 	printf("%d", sum);
 
 	return 0;
