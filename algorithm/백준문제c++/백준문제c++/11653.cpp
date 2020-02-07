b099a19faa3a60a43f03f9528ab825dfaa66d4c0
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2020-02-07 20:14:18 +0900

    백준 11653

---
+++ algorithm/백준문제c++/백준문제c++/11653.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int temp = N;
+
+	int is_prime = 0;
+
+	vector<int> result;
+
+	for (int i = 2; i < N; )
+	{
+		if (temp % i == 0)
+		{
+			result.push_back(i);
+			temp /= i;
+			is_prime++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	if (!is_prime)
+	{
+		printf("%d", N);
+	}
+	else
+	{
+		for (auto R : result)
+		{
+			printf("%d\n", R);
+		}
+	}
+
+	return 0;
+}
